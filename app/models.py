550f12db461ebbf4fcd2ef1296b6a004ad61f8e2
Nick DeMarco <6326614+ndemarco@users.noreply.github.com>
2025-06-06 08:55:11 -0400

    Restructure flask app initialization

--- app/models.py
+++ app/models.py
@@ -1,8 +1,6 @@
-from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime, timezone
 from sqlalchemy import UniqueConstraint
-
-db = SQLAlchemy()
+from app import db
 
 class Parts(db.Model):
     id = db.Column(db.Integer, primary_key=True)
